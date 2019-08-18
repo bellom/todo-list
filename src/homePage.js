baf2b47bc15ba55686f3b936833a24505e223649
Saheed <sdrfngg640@gmail.com>
2019-08-18 12:44:16 +0100

    Add project content, todo content, and select project display

--- src/homePage.js
+++ src/homePage.js
@@ -1,8 +1,14 @@
 import navbar from "./navbar";
 import projectLogic from "./projectLogic"
+import projectContentDom from "./projectContent";
+import todoContentDom from "./todoContentDom";
+import selectProjectsDisplay from "./selectProject";
 
 const homePage = () => {
     navbar();
+    projectContentDom();
+    todoContentDom();
+    selectProjectsDisplay();
     projectLogic.display();
 }
 
