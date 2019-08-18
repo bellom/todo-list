6baac6d5986409978e2ab384f7a0d9e0a16a918b
Saheed <sdrfngg640@gmail.com>
2019-08-18 20:54:38 +0100

    Update todo item project variable to projectOwn

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,5 +1,5 @@
-const todoItem = (title, description, dueDate, priority, project) => {
-    return { title, description, dueDate, priority, project }
+const todoItem = (title, description, dueDate, priority, projectOwn) => {
+    return { title, description, dueDate, priority, projectOwn }
 }
 
 export default todoItem;
