6a84e74a4a6b96afde43746a967c81eb2835487a
Saheed <sdrfngg640@gmail.com>
2019-08-18 20:51:17 +0100

    Style navbar text

--- src/navbar.js
+++ src/navbar.js
@@ -2,7 +2,7 @@ import projectLogic from "./projectLogic";
 
 const navbar = () => {
     const nav = document.getElementById("nav");
-    nav.innerHTML =    `<a class="navbar-brand text-white mx-auto" href="#">TODO LIST</a>`;    
+    nav.innerHTML =    `<a class="navbar-brand text-white mx-auto font-weight-bold" href="#">TODO LIST</a>`;    
 }
 
 export default navbar;
