0d60ab56fa30dcd49fba03105f7b1cdc3a982068
Saheed <sdrfngg640@gmail.com>
2019-08-23 16:29:47 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,9 @@
-const path = require("path");
-const CircularDependencyPlugin = require('circular-dependency-plugin')
+const path = require('path');
 
-    module.exports = {
-        entry: "./src/index.js",
-        output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new CircularDependencyPlugin({
-          // exclude detection of files based on a RegExp
-          exclude: /a\.js|node_modules/,
-          // add errors to webpack instead of warnings
-          failOnError: true,
-          // allow import cycles that include an asyncronous import,
-          // e.g. via import(/* webpackMode: "weak" */ './file.js')
-          allowAsyncCycles: false,
-          // set the current working directory for displaying module paths
-          cwd: process.cwd(),
-        })
-      ]
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
