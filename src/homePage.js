6be2c524a3ab1462f681af8d4e38860ee94b0fad
Saheed <sdrfngg640@gmail.com>
2019-08-23 16:47:33 +0100

    Fix linter error

--- src/homePage.js
+++ src/homePage.js
@@ -3,7 +3,6 @@ import navbar from "./navbar";
 import projectLogic from "./projectLogic"
 import projectContentDom from "./projectContent";
 import todoContentDom from "./todoContentDom";
-import selectProjectsDisplay from "./selectProject";
 import todoItemLogic from "./todoItemLogic";
 
 const homePage = () => {
