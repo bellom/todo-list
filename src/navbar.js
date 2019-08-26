5face7db74011e231f219d6fbaa5feb9f8096356
Saheed <sdrfngg640@gmail.com>
2019-08-26 10:37:43 +0100

    Move static contents to index.html

--- src/navbar.js
+++
@@ -1,8 +0,0 @@
-/* global document */
-
-const navbar = () => {
-    const nav = document.getElementById("nav");
-    nav.innerHTML =    `<a class="navbar-brand text-white mx-auto font-weight-bold" href="#">TODO LIST</a>`;    
-}
-
-export default navbar;
